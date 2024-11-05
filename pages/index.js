8f2b3fe7b6adddff578b53b9171755d1a3ee3cb7
Daniel Mouris <dgmouris@gmail.com>
2024-11-05 14:41:37 -0700

    Changed title

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ export default function Home() {
       <AppBar position="relative">
         <Toolbar>
           <Typography variant="h6" color="inherit" noWrap>
-            We Love Quotes
+            {"Great Breaking Bad Quotes"}
           </Typography>
         </Toolbar>
       </AppBar>
